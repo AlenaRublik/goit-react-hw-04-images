9ff4456ce7f84b6c4e966c2960ea0c75f357bf76
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-05-27 14:34:28 +0300

    bugfix

--- src/components/Text/Text.styled.jsx
+++ src/components/Text/Text.styled.jsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const P = styled.p`
   margin: 40px auto;
-  font-size: 72px;
+  font-size: 70px;
   text-align: center;
   background-color: #dede74;
 `;
